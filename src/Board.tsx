adec02f77bcd7f1816feae49b40dd00f5688e8cf
Annie1790 <budayaniko@outlook.com>
2023-05-22 15:41:01 +0100

    now can send message to server

--- src/Board.tsx
+++ src/Board.tsx
@@ -1,75 +1,52 @@
 import "./style/Board.scss"
 
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
-Date.now()
-
-const date = new Date();
-const todayDate = date.getFullYear() + '-' + (date.getMonth() + 1) + '-' + date.getDate();
-const todayTime = date.getHours() + ":" + date.getMinutes()
-
-const testData: testData = {
-    1: {
-        name: "Alex",
-        msg: "hello",
-        date: todayDate + " " + todayTime
-    },
-    2: {
-        name: "Aniko",
-        msg: "what",
-        date: todayDate + " " + todayTime
-    },
-    3: {
-        name: "Bela",
-        msg: "I am your father",
-        date: todayDate + " " + todayTime
-    },
-    4: {
-        name: "Father",
-        msg: "Mother",
-        date: todayDate + " " + todayTime
-    },
-    5: {
-        name: "yes",
-        msg: "no",
-        date: todayDate + " " + todayTime
-    },
-    6: {
-        name: "what",
-        msg: "yes",
-        date: todayDate + " " + todayTime
-    },
-    7: {
-        name: "jvosvjdflioivdf",
-        msg: "jfsikjdsilkvhsdli",
-        date: todayDate + " " + todayTime
-    },
-    8: {
-        name: "fjisdlvjdsoisd",
-        msg: "iosidsjifdjijakdsjkdsfajkljkalksdfjklajfkakfasjkdljkfldsfjskdl",
-        date: todayDate + " " + todayTime
-    }
-}
-
-interface testData {
+interface Data {
     [key: number]: {
         name: string;
         msg: string;
         date: string;
+        color: string;
     }
 }
 
 function Board() {
 
-    let [data, setData] = useState(testData);
+    useEffect(() => {
+        const interval = setInterval(() => {
+            retrieveMsg();
+        }, 1000);
+        return () => clearInterval(interval)
+    }, [])
 
-    //fetch, data equals to testData
+    let [data, setData] = useState({} as Data);
+
+    async function retrieveMsg() {
+        try {
+            const response = await fetch("http://localhost:4000/", {
+                method: "GET",
+                mode: "cors",
+                headers: {
+                    "Content-type": "application/json"
+                }
+            })
+            if (response.ok) {
+                const result = await response.json();
+                console.log(`GET data ${JSON.stringify(result)}`);
+                setData(result);
+            }
+        }
+        catch (error) {
+            console.error(error)
+        }
+    }
 
     function returnUserNameMsg() {
         const arr = [];
         for (let segment in data) {
             arr.push(
-                < div className="msgBubble" style={randomColor()}>
+                < div className="msgBubble">
                     <span className="msgName">{data[segment].name}</span>
                     <span className="msgDate">{data[segment].date}</span>
                     <p className="msgBody">{data[segment].msg}</p>
@@ -77,15 +54,6 @@ function Board() {
             )
         }
         return arr;
-    }
-
-    function randomColor() {
-        const hex = ["#cbc0d3","#e07a5f","#FF99C8", "#FCF6BD", "#D0F4DE","#A9DEF9", "#E4C1F9"]
-        const result = Math.floor(Math.random()* hex.length);
-        const style:React.CSSProperties = {
-            backgroundColor: hex[result]
-        };
-        return style;
     }
 
     return (
--- src/Input.tsx
+++ src/Input.tsx
@@ -1,13 +1,63 @@
+import { useEffect, useState } from "react";
 import "./style/Input.scss"
 
+
 function Input() {
+    const [data, setData]: any = useState<handleSub>();
+    const [counter, setCounter]: any = useState(1);
+    const date = new Date();
+    const todayDate = date.getFullYear() + '-' + (date.getMonth() + 1) + '-' + date.getDate();
+    const todayTime = date.getHours() + ":" + date.getMinutes();
+
+    async function sendMsg(data: Object) {
+        try {
+            const response = await fetch("http://localhost:4000/", {
+                method: "POST",
+                mode: "cors",
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify(data)
+            })
+            const result = await response.json();
+            console.log(`POST data: ${result}`)
+        }
+        catch (error) {
+            console.error(error)
+        }
+    }
+
+    interface handleSub {
+        [key: number]: {
+            name: string;
+            msg: string;
+            date: string;
+        }
+    }
+
+    const handleSub = (event: any) => {
+        event.preventDefault();
+        setCounter(counter + 1);
+        const result = {
+            ...data,
+            [counter]: {
+                name: event.target.name.value,
+                msg: event.target.msg.value,
+                date: `${todayDate} ${todayTime}`
+            }
+        }
+        setData(result);
+        sendMsg(result);
+        event.target.name.value = "";
+        event.target.msg.value = "";
+    }
 
     return (
-            <form id="inputField" method="post">
-                <input id="username" name="name" placeholder="Your name"></input>
-                <input id="message" name="msg" placeholder="message"></input>
-                <button id="submit"><svg xmlns="http://www.w3.org/2000/svg" height="40" viewBox="0 96 960 960" width="40"><path d="M780 856V682q0-54-38-92t-92-38H234l154 154-42 42-226-226 226-226 42 42-154 154h416q78 0 134 55.5T840 682v174h-60Z" /></svg></button>
-            </form>
+        <form id="inputField" onSubmit={(event) => { handleSub(event); }}>
+            <input id="username" name="name" placeholder="Your name"></input>
+            <input id="message" name="msg" placeholder="message"></input>
+            <button id="submit" type="submit" ><svg xmlns="http://www.w3.org/2000/svg" height="40" viewBox="0 96 960 960" width="40"><path d="M780 856V682q0-54-38-92t-92-38H234l154 154-42 42-226-226 226-226 42 42-154 154h416q78 0 134 55.5T840 682v174h-60Z" /></svg></button>
+        </form>
     )
 }
 
